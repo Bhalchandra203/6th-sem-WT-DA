fa74a932d60d79a33d864303a777177cb681b45f
Bhalchandra khairnar <145736562+Bhalchandra203@users.noreply.github.com>
2024-04-11 15:25:31 +0530

    Update result.php

--- slip2_1/result.php
+++ slip2_1/result.php
@@ -1,35 +1,9 @@
-<?php 
+<?php
 
-if(isset($_COOKIE['font-style']))
-{
-    $font_style =  $_COOKIE['font-style'] ;
-}
-if(isset($_COOKIE['font-size']))
-{
-    $font_size =  $_COOKIE['font-size'] ;
-}
-if(isset($_COOKIE['font-color']))
-{
-    $font_color =  $_COOKIE['font-color'] ;
-}
+    $style = $_COOKIE['style'];
+    $size = $_COOKIE['size'];
+    $color = $_COOKIE['color'];
+    $bgColor = $_COOKIE['bgColor'];
+
+    echo "<h1 style='font-family: $style; font-size: $size; color: $color; background-color: $bgColor;'>How are you doing?</h1>";
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title></title>
-    <style>
-        p{
-            font-size: <?php echo $font_size ?> px;
-            font-style: <?php echo $font_style ?>;
-            color: <?php echo $font_color?>;
-
-        }
-    </style>
-</head>
-<body>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus ratione consectetur, ipsa ex ipsum doloribus mollitia ad tempore quidem repudiandae fuga. </p>
-</body>
-</html>
