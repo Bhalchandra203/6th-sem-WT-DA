d4a4b24f720bdb2b6926040bc200c15e0f14d3ce
Bhalchandra khairnar <145736562+Bhalchandra203@users.noreply.github.com>
2024-04-11 15:38:19 +0530

    Update index1.php

--- slip4_1/index1.php
+++ slip4_1/index1.php
@@ -1,38 +1,30 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-
-<body>
-    <form action="#" method="POST">
-        <h4>Enter Basic details of employee(page-1)</h4>
-        <label>Enter employee number :</label>
-        <input type="number" name="eno">
-        <br>
-        <label>Enter employee name :</label>
-        <input type="text" name="ename">
-        <br>
-        <label>Enter employee address:</label>
-        <textarea name="address">
-        </textarea>
-
-        <br>
-        <button type="submit">Submit</button>
-        <button type="reset">reset</button>
-    </form>
-</body>
-</html>
-
 <?php
-session_start();
-
-if (isset($_POST['eno']) && isset($_POST['ename']) && isset($_POST['address'])) {
-    $_SESSION['eno'] = $_POST['eno'];
-    $_SESSION['ename'] = $_POST['ename'];
-    $_SESSION['address'] = $_POST['address'];
-    echo "<h5>First Page details saved successfully <a href='index2.php'>page-2</a></h5> ";
-}
+    session_start();
+    
+    if(isset($_POST['submit'])) {
+        $eno = $_POST['eno'];
+        $ename = $_POST['ename'];
+        $address = $_POST['address'];
+        
+        $_SESSION['eno'] = $eno;
+        $_SESSION['ename'] = $ename;
+        $_SESSION['address'] = $address;
+        
+        header("Location: index2.php");
+        exit();
+    }
 ?>
+
+<html>
+    <head>
+        <title>title</title>
+    </head>
+    <body>
+        <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
+            Enter eno     :<input type="text" name="eno"><br>
+            Enter ename   :<input type="text" name="ename"><br>
+            Enter address :<input type="text" name="address"><br>
+            <input type="submit" name="submit" value="submit">
+        </form>
+    </body>
+</html>
