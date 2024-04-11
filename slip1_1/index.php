36e9e324723984b6fc13e57d22651d29ef0dcb89
Bhalchandra khairnar <145736562+Bhalchandra203@users.noreply.github.com>
2024-04-11 15:15:20 +0530

    Update index.php

--- slip1_1/index.php
+++ slip1_1/index.php
@@ -1,16 +1,10 @@
 <?php
-session_start();
+    session_start();
 
-
-if (!isset($_SESSION['visit_count'])) 
-{
-    $_SESSION['visit_count'] = 1; // Initialize visit count to 1 if not set
-}
-else 
-{
-    $_SESSION['visit_count']++; // Increment visit count if already set
-}
-
-// Display the visit count
-echo "You have visited this page " . $_SESSION['visit_count'] . " times.";
+    if(!isset($_SESSION['pageCnt']))
+        $_SESSION['pageCnt'] = 1;
+    else
+        $_SESSION['pageCnt']++;
+        
+    echo "Page is accessed ".$_SESSION['pageCnt'] ." times.";
 ?>
